b311ae90221caf7791a502995aed7dca9a93b6c1
Josh Roy <10731363+JRoy@users.noreply.github.com>
2025-01-16 11:11:46 -0500

    probably works?

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# caddydns-infoblox
-Caddy module: dns.providers.infoblox
+Infoblox module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with Infoblox accounts.
+
+## Caddy module name
+
+```
+dns.providers.infoblox
+```
+
+## Configuration
+
+### JSON Example
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuers/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "infoblox",
+				"host": "INFOBLOX_HOST",
+                "version": "INFOBLOX_VERSION",
+                "username": "INFOBLOX_USERNAME",
+                "password": "INFOBLOX_PASSWORD"
+			}
+		}
+	}
+}
+```
+
+### Caddyfile Examples
+
+```Caddyfile
+tls {
+	dns infoblox {
+		host {env.INFOBLOX_HOST}
+		version {env.INFOBLOX_VERSION}
+		username {env.INFOBLOX_USERNAME}
+		password {env.INFOBLOX_PASSWORD}
+	}
+}
+```
---
+++ module.go
@@ -0,0 +1,78 @@
+package infoblox
+
+import (
+	infoblox "github.com/JRoy/libdns-infoblox"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+)
+
+type Provider struct{ *infoblox.Provider }
+
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.infoblox",
+		New: func() caddy.Module { return &Provider{new(infoblox.Provider)} },
+	}
+}
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+func (p *Provider) Provision(ctx caddy.Context) error {
+	p.Provider.Host = caddy.NewReplacer().ReplaceAll(p.Provider.Host, "")
+	p.Provider.Version = caddy.NewReplacer().ReplaceAll(p.Provider.Version, "")
+	p.Provider.Username = caddy.NewReplacer().ReplaceAll(p.Provider.Username, "")
+	p.Provider.Password = caddy.NewReplacer().ReplaceAll(p.Provider.Password, "")
+	return nil
+}
+
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "host":
+				if d.NextArg() {
+					p.Provider.Host = d.Val()
+				} else {
+					return d.ArgErr()
+				}
+			case "version":
+				if d.NextArg() {
+					p.Provider.Version = d.Val()
+				} else {
+					return d.ArgErr()
+				}
+			case "username":
+				if d.NextArg() {
+					p.Provider.Username = d.Val()
+				} else {
+					return d.ArgErr()
+				}
+			case "password":
+				if d.NextArg() {
+					p.Provider.Password = d.Val()
+				} else {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+
+	if p.Provider.Host == "" || p.Provider.Version == "" || p.Provider.Username == "" || p.Provider.Password == "" {
+		return d.Err("missing config!")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
